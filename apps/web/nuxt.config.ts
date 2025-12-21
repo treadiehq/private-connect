a18b476210d89f5155c4d1a52687470c0891a475
Dante Lex <dantelex2@gmail.com>
2025-12-21 11:58:47 -0500

    Add Seline analytics

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -33,9 +33,9 @@ export default defineNuxtConfig({
       ],
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-        // { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-        // { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
-        // { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Instrument+Sans:wght@400;500;600;700&family=JetBrains+Mono:wght@400;500&display=swap' },
+      ],
+      script: [
+        { src: 'https://cdn.seline.com/seline.js', async: true, 'data-token': 'd5fd31a3538303d' },
       ],
     },
   },
